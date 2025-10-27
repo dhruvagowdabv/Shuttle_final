e959a8ad369d2e4235b41081b4cdc50cb6766769
dhruvagowdabv <dhruvagit05@gmail.com>
2025-10-27 13:40:51 +0530

    procfile

--- project/project/settings.py
+++ project/project/settings.py
@@ -13,7 +13,8 @@ SECRET_KEY = os.environ.get(
 DEBUG = os.environ.get("DEBUG", "True") == "True"
 
 # ---------------- ALLOWED HOSTS ----------------
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '192.168.43.145']
+
 RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
